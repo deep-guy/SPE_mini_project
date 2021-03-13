2a3ebbadbad8f13fcc1877b398c6182194a69c99
Deep Inder Mohan <mohan.deepinder@qgmail.com>
2021-03-13 11:45:48 +0530

    Added JUnit tests in the test directory

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,20 +1,62 @@
-package calculator;
-
-import static org.junit.Assert.assertTrue;
-
+import calculator.Calculator;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class CalculatorTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class CalculatorTest {
+    private static final double DELTA = 1e-15;
+    Calculator calculator = new Calculator();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void squareRootTruePositive(){
+        assertEquals("Square root of double number for True Positive", 4.0, calculator.squareRoot(16.0), DELTA);
+        assertEquals("Square root of double number for True Positive", 3.0, calculator.squareRoot(9.0), DELTA);
     }
+
+    // @Test
+    // public void addingFalsePositive(){
+    //     assertNotEquals("Adding two integer numbers for False Positive", 6, calculator.add(2, 2), DELTA);
+    //     assertNotEquals("Adding two double numbers for False Positive", 7.3, calculator.add(2.1, 3.2), DELTA);
+    // }
+
+    // @Test
+    // public void subtractingTruePositive(){
+    //     assertEquals("Subtracting two integer numbers for True Positive", 0, calculator.subtract(2, 2), DELTA);
+    //     assertEquals("Subtracting two double numbers for True Positive", -1.1, calculator.subtract(2.1, 3.2), DELTA);
+    // }
+
+    // @Test
+    // public void subtractingFalsePositive(){
+    //     assertNotEquals("Subtracting two integer numbers for False Positive", 6, calculator.subtract(2, 2), DELTA);
+    //     assertNotEquals("Subtracting two double numbers for False Positive", -7.3, calculator.subtract(2.1, 3.2), DELTA);
+    // }
+
+    // @Test
+    // public void multiplyingTruePositive(){
+    //     assertEquals("Multiplying two integer numbers for True Positive", 4, calculator.multiply(2, 2), DELTA);
+    //     assertEquals("Multiplying two double numbers for True Positive", 6.72, calculator.multiply(2.1, 3.2), DELTA);
+    // }
+
+    // @Test
+    // public void multiplyingFalsePositive(){
+    //     assertNotEquals("Multiplying two integer numbers for False Positive", 6, calculator.multiply(2, 2), DELTA);
+    //     assertNotEquals("Multiplying two double numbers for False Positive", 7.3, calculator.multiply(2.1, 3.2), DELTA);
+    // }
+
+    // @Test
+    // public void dividingTruePositive(){
+    //     assertEquals("Dividing two integer numbers for True Positive", 1, calculator.divide(2, 2), DELTA);
+    //     assertEquals("Dividing two double numbers for True Positive", Double.NaN, calculator.divide(0, 0), DELTA);
+    //     assertEquals("Dividing two double numbers for True Positive", Double.POSITIVE_INFINITY, calculator.divide(1, 0), DELTA);
+    //     assertEquals("Dividing two double numbers for True Positive", Double.NEGATIVE_INFINITY, calculator.divide(-1, 0), DELTA);
+    // }
+
+    // @Test
+    // public void dividingFalsePositive(){
+    //     assertNotEquals("Dividing two integer numbers for True Positive", 1, calculator.divide(2.3, 1.3), DELTA);
+    //     assertNotEquals("Dividing two double numbers for True Positive", 0, calculator.divide(0, 0), DELTA);
+    //     assertNotEquals("Dividing two double numbers for True Positive", 1, calculator.divide(1, 0), DELTA);
+    //     assertNotEquals("Dividing two double numbers for True Positive", -1, calculator.divide(-1, 0), DELTA);
+    // }
+
+
 }
